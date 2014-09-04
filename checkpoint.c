03f5d68b08f3ec8283de1f1df009ad9dcb76cc8b
Pradeep Fernando <pradeepfn@gmail.com>
2014-09-04 13:24:14 -0400

    working code. need to improve with queue based logging..

--- checkpoint.c
+++ checkpoint.c
@@ -20,7 +20,7 @@ int offset;
 
 
 void init(){
-    offset = 0; // pointing to starting address;
+    offset = -1; // pointing to starting address;
 }
 
 /*
@@ -59,41 +59,51 @@ extern checkpoint_t *get_latest_version(int id){
 	//first restore the global counter
 	offset = *((int *)int_memory);
 	checkpoint_t * current = get_meta(meta_memory,offset);
-	return get_latest_version(meta_memory, current,id);
+	return get_latest_version1(meta_memory, current,id);
 }
 
-checkpoint_t *get_latest_version(void *base_addr, checkpoint_t *head, int id){
+checkpoint_t *get_latest_version1(void *base_addr, checkpoint_t *head, int id){
 	checkpoint_t *current = head;
-	while(!is_empty_checkpoint() & current->prv_offset != -1){
-		checkpoint_t *ptr = get_meta(base_addr,current->offset);
+	int temp_offset = offset;
+	while(offset >= 0){
+		checkpoint_t *ptr = get_meta(base_addr,temp_offset);
 		if((ptr->id) == id){
 			return ptr;
 		}
+		temp_offset = ptr->prv_offset;
 	}
 	return NULL;//to-do, handle exception cases
 
 }
 
 extern void checkpoint(int id, int version, size_t size, void *data){
-	checkpoint(meta_memory,offset,id,version,size,data);
+	checkpoint2(meta_memory,offset,id,version,size,data);
 	return;	
 }
 
-void checkpoint(void *base_addr, size_t last_meta_offset, int id, int version, size_t size, void *data){
-	checkpoint_t *last_meta = get_meta(base_addr, last_meta_offset);
-	void *start_addr = get_start_addr(base_addr, last_meta);
+void checkpoint2(void *base_addr, size_t last_meta_offset, int id, int version, size_t size, void *data){
 	checkpoint_t chkpt;
+	void *start_addr;
 	chkpt.id = id;
 	chkpt.version = version;
 	chkpt.data_size = size;
-	chkpt.prv_offset = last_meta_offset;
-	chkpt.offset = last_meta->offset + sizeof(checkpoint_t)+last_met->data_size;
-	checkpoint(start_addr, &chkpt,data);
+	if(offset != -1 ){
+		checkpoint_t *last_meta = get_meta(base_addr, last_meta_offset);
+		start_addr = get_start_addr(base_addr, last_meta);
+		chkpt.prv_offset = last_meta_offset;
+		chkpt.offset = last_meta->offset + sizeof(checkpoint_t)+last_meta->data_size;
+	}else{
+		start_addr = meta_memory;
+		chkpt.prv_offset = -1;
+		chkpt.offset = 0;
+	}
+	print_data(&chkpt);
+	checkpoint1(start_addr, &chkpt,data);
 	return;
 }
 
 
-void checkpoint(void *start_addr, checkpoint_t *chkpt, void *data){ 
+void checkpoint1(void *start_addr, checkpoint_t *chkpt, void *data){ 
 	//copy the metadata 
 	memcpy(start_addr,chkpt,sizeof(checkpoint_t));
 	//copy the actual value after metadata.
@@ -105,13 +115,13 @@ void checkpoint(void *start_addr, checkpoint_t *chkpt, void *data){
 }        
 
 void *get_start_addr(void *base_addr,checkpoint_t *last_meta){
-	size_t tot_offset = last_meta->offset + sizeof(checkpoint_t) + last-meta->data_size;
+	size_t tot_offset = last_meta->offset + sizeof(checkpoint_t) + last_meta->data_size;
 	char *next_start_addr = (char *)base_addr+tot_offset;
 	return (void *)next_start_addr;
 }
 
 checkpoint_t *get_meta(void *base_addr,size_t offset){
-	checkpoint *ptr = (checkpoint_t *)(((char *)baseaddr) + offset);
+	checkpoint_t *ptr = (checkpoint_t *)(((char *)base_addr) + offset);
 	return ptr; 
 }
 
@@ -126,38 +136,45 @@ void *get_addr(void *base_addr, size_t offset){
 }
 
 int get_new_offset(int offset, size_t data_size){
-	int temp = offset + sizeof(checkpoint_t) + size; 
+	int temp = offset + sizeof(checkpoint_t) + data_size; 
 	return temp; 
 }
 
+void print_data(checkpoint_t *chkptr){
+	printf("id : %d\n", chkptr->id);
+	printf("version : %d\n", chkptr->version);
+	printf("is_valid : %d\n", chkptr->is_valid);
+	printf("data_size : %zd\n", chkptr->data_size);
+	printf("prev_offset : %zd\n", chkptr->prv_offset);
+	printf("offset : %zd\n", chkptr->offset);
+	return;
+}
+
 int main(int argc, char *argv[]){
-	void *start_address = malloc(sizeof(checkpoint_t)*20);
-	
-	char ca[]="Hello world.From the copy buffer";
-	checkpoint_t chk;
-	chk.ch_id = 1;
-	chk.data_size = sizeof(ca);
-	chk.offset = 0;
-	checkpoint(start_address,&chk,ca);
+	printf("Checkpoint testing...\n");
+	checkpoint_t *ptr;
+	init();
+	if(!is_chkpoint_present()){
+		checkpoint_init();
+		char ch[] = "Hello worldi";
+	    checkpoint(1, 1, sizeof(ch),ch);
+		char ch2[] = "Pradeep Fernando";
+		checkpoint(2,2,sizeof(ch2),ch2);			
+		
+		ptr = get_meta(meta_memory,0);
+		print_data(ptr);
+		ptr = get_meta(meta_memory,offset);
+		print_data(ptr);
+		return 0;
+	}else{
+		checkpoint_init();
+        ptr = get_latest_version(2);
+		print_data(ptr); 
+        ptr = get_latest_version(1);
+		print_data(ptr); 
+		return 0;
+	}
 
-    int calc = chk.data_size + chk.offset + sizeof(checkpoint_t);
-
-	char ca2[] = "second hello";
-	checkpoint_t chk2;
-	chk2.ch_id = 2;
-	chk2.data_size = sizeof(ca2);
-	chk2.offset = calc;
-	char * offset_calc = (char *)start_address + calc;
-	checkpoint(offset_calc,&chk2,ca2);
-
-	checkpoint_t *chk_ptr;
-	chk_ptr = (checkpoint_t *)start_address;
-	printf("data size %zd \n",chk_ptr->data_size);	
-	printf("offset %zd \n",chk.offset);	
-	printf("offset %zd \n",chk_ptr->offset);	
-	char *c = (char *)get_data_addr(start_address,&chk);
-	printf("%s\n", c);
-	return 0;
 }
 
 
--- checkpoint.h
+++ checkpoint.h
@@ -6,7 +6,7 @@ typedef enum { false, true } bool;
 // meta data structure used during data persistance
 // id + version defines a unique structure.
 typedef struct checkpoint{
-	int ch_id;
+	int id;
 	int version;
 	bool is_valid;
 	size_t data_size;
@@ -16,9 +16,18 @@ typedef struct checkpoint{
 
 
 bool is_chkpoint_present();
-int rtrv_last_state();
-void checkpoint(void *start_address, checkpoint_t *chkpt, void *data);
 void map_memory_file();
 void init();
+checkpoint_t *get_meta(void *base_addr,size_t offset);
+checkpoint_t *get_latest_version(int id);
+checkpoint_t *get_latest_version1(void *base_addr, checkpoint_t *head, int id);
+void checkpoint(int id, int version, size_t size, void *data);
+void checkpoint2(void *base_addr, size_t last_meta_offset, int id, int version, size_t size, void *data);
+void checkpoint1(void *start_addr, checkpoint_t *chkpt, void *data);
+void *get_start_addr(void *base_addr,checkpoint_t *last_meta);
+void *get_data_addr(void *base_addr, checkpoint_t *chkptr);
+void *get_addr(void *base_addr, size_t offset);
+int get_new_offset(int offset, size_t data_size);
+void print_data(checkpoint_t *chkptr);
 
 #endif
